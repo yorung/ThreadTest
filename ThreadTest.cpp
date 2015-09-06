7e57b37057d7e6ee8bd08c92f279ec92fad4c8ac
yorung <yorung@gmail.com>
2015-09-06 22:30:15 +0900

    speed test

--- ThreadTest.cpp
+++ ThreadTest.cpp
@@ -3,6 +3,7 @@
 
 #include "stdafx.h"
 #include <thread>
+#include <chrono>
 
 int a = 0;
 int egis;
@@ -14,6 +15,24 @@ void Lock()
 		xchg eax, [egis]
 		test eax, eax
 		jnz retry
+	done:
+	}
+}
+
+void Lock2()
+{
+	__asm {
+		mov eax, 1;
+	retry:
+		xchg eax, [egis]
+		test eax, eax
+		jz done
+		retry_mov :
+		mov ebx, [egis]
+		test ebx, ebx
+		jnz retry_mov
+		jmp retry
+	done:
 	}
 }
 
@@ -22,6 +41,13 @@ void Unlock()
 	__asm {
 		mov eax, 0;
 		xchg eax, [egis]
+	}
+}
+
+void Unlock2()
+{
+	__asm {
+		mov[egis], 0;
 	}
 }
 
@@ -40,13 +66,25 @@ void ThreadMain()
 	}
 }
 
+double GetTime()
+{
+	static auto start = std::chrono::high_resolution_clock::now();
+	auto now = std::chrono::high_resolution_clock::now();
+	return std::chrono::duration_cast<std::chrono::duration<double, std::ratio<1, 1>>>(now - start).count();
+}
+
 int main()
 {
+	double begin = GetTime();
 	std::thread t1(ThreadMain);
 	std::thread t2(ThreadMain);
 	std::thread t3(ThreadMain);
 	t1.join();
 	t2.join();
 	t3.join();
+	double end = GetTime();
+
+	printf("elapsed: %f", end - begin);
+
 	return 0;
 }
